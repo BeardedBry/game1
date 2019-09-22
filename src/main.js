4c4a338184d0b2b5ab027f479c8636b2f86eebff
Brian Burkard <brianburkard@gmail.com>
2019-09-22 16:22:24 -0400

    cat clicker

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
-const myGuess = Math.floor(Math.random() * 20) +1;
-let guesses = 0;
-let guess;
+let clickers = 50;
+let startTime = Date.now();
 
-while (guess !== myGuess) {
-    guess = parseInt(prompt("What number am I thinking of?"), 10);
-    guesses++;
-    if (guess < myGuess) {
-    alert("Higher.");
-    } else if (guess > myGuess) {
-    alert("Lower.");
-    }
-    }
-    alert(`Well done! You got it in ${ guesses }!`);
+// position
+function sync(dom, pos) {
+  dom.style.left = `${pos.x}px`;
+  dom.style.top = `${pos.y}px`;
+}
+//`${pos.x}px`
+
+function addClicker() {
+    const pos = {
+        x: Math.random() * 500,
+        y: Math.random() * 300,
+    };
+    const img = new Image();
+    img.src = "res/images/cat.png";
+    img.style.position = "absolute";
+    img.addEventListener("click", removeClicker, false);
+
+    document.querySelector('#board').appendChild(img);
+    sync(img, pos);
+}
+
+function removeClicker(e){
+    e.target.parentElement.removeChild(e.target);
+}
+
+let i = 0;
+
+while(i < 10){
+    addClicker();
+    i++;
+}
